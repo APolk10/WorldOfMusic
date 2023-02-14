0247ceb420ef04dc48225868729c73d62c6a9344
Adam Polk <adampolk91@gmail.com>
2023-02-14 14:05:35 -0600

    fixes tile padding for buttons/text

--- src/components/musicTile.tsx
+++ src/components/musicTile.tsx
@@ -23,22 +23,27 @@ const MusicTile: React.FC<TileProps> = ({ artist, nameOfCountry }) => {
   return (
     <div className='tileBox'>
        <div className='tile'>
-        <div className='tileInfoHeader'>Country of Origin:
-          <p className='tileInfoData'>{nameOfCountry}</p>
+        <div className='tileInner'>
+          <div className='tileInfoHeader'>Country of Origin:
+            <p className='tileInfoData'>{nameOfCountry}</p>
+          </div>
+          <div className='tileInfoHeader'>Full Name:
+            <p className='tileInfoData'>{artist.name}</p>
+          </div>
+          <div className='tileInfoHeader'>Alternative Name:
+            <p className='tileInfoData'>{artist.aliases ? artist.aliases[0].name : 'unknown'}</p>
+          </div>
+          <div className='tileInfoHeader'>Solo or Group:
+            <p className='tileInfoData'>{artist.type === 'Person' ? 'solo' : 'group'}</p>
+          </div>
         </div>
-        <div className='tileInfoHeader'>Full Name:
-          <p className='tileInfoData'>{artist.name}</p>
+        <div>
+          <div className='buttonsDiv'>
+            <button type='submit' className='spotifyButton' onClick={redirectToSpotify}>Find Their Music</button>
+            <button className='addFavButton'>Favorite This</button>
+          </div>
         </div>
-        <div className='tileInfoHeader'>Alternative Name:
-          <p className='tileInfoData'>{artist.aliases ? artist.aliases[0].name : 'unknown'}</p>
-        </div>
-        <div className='tileInfoHeader'>Solo or Group:
-          <p className='tileInfoData'>{artist.type === 'Person' ? 'solo' : 'group'}</p>
-        </div>
-        <div className='buttonsDiv'>
-          <button type='submit' className='spotifyButton' onClick={redirectToSpotify}>Find Their Music</button>
-          <button className='addFavButton'>Favorite This</button>
-        </div>
+
        </div>
     </div>
   )
