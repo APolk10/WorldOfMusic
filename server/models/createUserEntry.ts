349b0370f1452c0b0383b1e1b2df40eb4571d0af
Adam Polk <adampolk91@gmail.com>
2023-02-28 18:09:11 -0600

    removes some console logs

--- server/models/createUserEntry.ts
+++ server/models/createUserEntry.ts
@@ -3,7 +3,6 @@ import checkUser from './checkUser';
 
 const createUserEntry = function(user: string, session: string) {
   // check user, and if not present, create user
-  console.log(user, session);
   return db.query(`INSERT INTO users (username, session_id) VALUES ('${user}', '${session}');`)
 }
 
--- server/models/trackClick.ts
+++ server/models/trackClick.ts
@@ -2,7 +2,6 @@ import db from '../../database/index';
 
 const trackClick = function(country: string, iso: string) {
   // update the global counter for this country
-  console.log('trackClick activated');
   return db.query(`INSERT INTO countries (country_name, iso_code, clicks) VALUES ('${country}', '${iso}', 1) ON CONFLICT (country_name) DO UPDATE SET clicks = countries.clicks + excluded.clicks;`)
 }
 
