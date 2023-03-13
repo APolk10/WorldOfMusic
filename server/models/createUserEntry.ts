c9d86e6f20244a1230c34d535d8089254c5f5e27
Adam Polk <adampolk91@gmail.com>
2023-03-13 12:17:59 -0500

    fixes checkUser createUser query to have a placeholder variable for pin

--- server/models/createUserEntry.ts
+++ server/models/createUserEntry.ts
@@ -3,7 +3,7 @@ import checkUser from './checkUser';
 
 const createUserEntry = function(user: string, pin: number, session: string) {
   // check user, and if not present, create user
-  return db.query("INSERT INTO users (username, pin, session_id) VALUES ($1, $2);", [user, pin, session])
+  return db.query("INSERT INTO users (username, pin, session_id) VALUES ($1, $2, $3);", [user, pin, session])
 }
 
 export default createUserEntry;
