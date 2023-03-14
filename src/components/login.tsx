2654cecb6772a2a4e796b5c6ef0c4738d1666f91
Adam Polk <adampolk91@gmail.com>
2023-03-14 12:24:07 -0500

    reworks login logic to allow for new and existing users

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -8,7 +8,15 @@ interface loginProps {
 
 const Login: React.FC<loginProps> = ({ checkUserCredentials, flag }) => {
   const[username, setUsername] = useState('');
+  const[newUser, setNewUser] = useState(0);
   const[pin, setPin] = useState('');
+
+  const handleNewUserClick = () => {
+    setNewUser(1);
+  }
+  const handleExistingUserClick = () => {
+    setNewUser(2);
+  }
 
   const handleUsernameInput = (e: React.ChangeEvent<HTMLInputElement>):void  => {
     setUsername(e.target.value);
@@ -22,18 +30,42 @@ const Login: React.FC<loginProps> = ({ checkUserCredentials, flag }) => {
     checkUserCredentials(username, parseInt(pin));
   }
 
+  const renderLogin = () => {
+      if (newUser === 0) {
+        return <></>
+      }
+
+      if (newUser === 1) {
+        return (
+          <div>
+            <p>Please enter your username and pin number.</p>
+            <input type='input' onChange={handleUsernameInput} placeholder={'USERNAME'}></input>
+            { flag ? <p className='loginError'>This username is taken</p> : <></>}
+            <input type='input' onChange={handlePinInput} placeholder={'PIN'}></input>
+            <button type='button' onClick={handleSubmit}>Submit</button>
+          </div>
+        )
+      }
+      if (newUser === 2) {
+        return (
+          <div>
+            <p>If this is your first time, please provide a unique username and memorable pin number.</p>
+            <input type='input' onChange={handleUsernameInput} placeholder={'USERNAME'}></input>
+            { flag ? <p className='loginError'>This username is taken</p> : <></>}
+            <input type='input' onChange={handlePinInput} placeholder={'PIN'}></input>
+            <button type='button' onClick={handleSubmit}>Submit</button>
+          </div>
+        )
+      }
+  }
+
   return (
     <div className='login'>
-      <div>
-        <p>Please enter your username and pin number.</p>
-        <p>If this is your first time, please provide a unique username and memorable pin number.</p>
-      </div>
-      <div>
-        <input type='input' onChange={handleUsernameInput} placeholder={'USERNAME'}></input>
-        { flag ? <p className='loginError'>This username is taken</p> : <></>}
-        <input type='input' onChange={handlePinInput} placeholder={'PIN'}></input>
-        <button type='button' onClick={handleSubmit}>Submit</button>
-      </div>
+      <button type='button' onClick={handleNewUserClick}>New User</button>
+      <button type='button' onClick={handleExistingUserClick}>Existing User</button>
+      <>
+        {renderLogin()}
+      </>
     </div>
   )
 }
